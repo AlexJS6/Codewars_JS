e25a5c49f724596282afabebd1733ea842cfbfe3
AlexJS6 <alex.spiesberger@gmail.com>
2020-10-21 19:39:47 +0200

    Adding a 6 kata

--- kata_6.js
+++ kata_6.js
@@ -225,3 +225,11 @@ function findOutlier(integers){
         return even;
     }
 }
+
+
+https://www.codewars.com/kata/5262119038c0985a5b00029f
+const isPrime = num => {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false; 
+    return num > 1;
+}
