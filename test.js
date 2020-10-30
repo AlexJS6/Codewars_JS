a28de34144d4a412a9db07c99e6624762de561ba
AlexJS6 <alex.spiesberger@gmail.com>
2020-10-30 21:59:24 +0100

    Just a test

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 // LEARN METHODS: https://www.youtube.com/c/SteveGriffith-Prof3ssorSt3v3/search?query=method
-// LEARN MAp
-// LEARN Some
-// LEARN forEach
-// LEARN every
-// LEARN from
-// Learn reduce
-// LEARN filter
-// LEAR Includes
+// LEARN MAp     DONE
+// LEARN Some     DONE
+// LEARN forEach   DONE
+// LEARN every   DONE
+// LEARN from    DONE
+// Learn reduce   DONE
+// LEARN     DONE
+// LEAR Includes DONE
+// LEARN Create 
+// LEARN sort
