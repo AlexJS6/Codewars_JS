e44418b682ab809d6347d38fae6d40ae5afd3ccf
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-02 23:50:04 +0100

    Adding kata 5

--- kata_5.js
+++ kata_5.js
@@ -98,3 +98,27 @@ function validParentheses(parens){
   while (re.test(parens)) parens = parens.replace(re, "");
   return !parens;
 }
+
+
+https://www.codewars.com/kata/55c6126177c9441a570000cc
+function orderWeight(strng) {
+  return strng
+    .split(" ")
+    .map(function(v) {  
+      return {
+        val: v,
+        key: v.split("").reduce(function(prev, curr) {
+          return parseInt(prev) + parseInt(curr);
+        }, 0)
+      };
+    })
+    .sort(function(a, b) {
+      return a.key == b.key 
+        ? a.val.localeCompare(b.val)
+        : (a.key - b.key);
+    })
+    .map(function(v) {
+      return v.val;
+    })
+    .join(" ");
+}
--- test.js
+++ test.js
@@ -10,3 +10,27 @@
 // LEARN Create 
 // LEARN sort
 // change name
+
+function orderWeight(string) {
+    // Split string (' ')
+    new_arr = string.split(' '), sec_arr = [];
+    // get each to string 
+    // split each
+    new_arr.forEach(elem => {
+        arr = elem.split('');
+        let result = 0;
+        arr.forEach(num => {
+            result += parseInt(num);
+        });
+        sec_arr.push(result);
+        console.log(sec_arr)
+    });
+    
+    // parseInt them and sum them
+    // sort them and put in order into array
+
+}
+
+
+console.log(orderWeight("103 123 4444 99 2000"))//, "2000 103 123 4444 99")
+console.log(orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123"))//, "11 11 2000 10003 22 123 1234000 44444444 9999")
